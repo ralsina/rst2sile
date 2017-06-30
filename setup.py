a738a236db9c7e1b5d0f30a7033714c7391b3a4f
Roberto Alsina <ralsina@onapsis.com>
2017-06-30 17:08:34 -0300

    0.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
     name='rst2sile',
-    version='0.1',
+    version='0.2',
     install_requires=open('requirements.txt').readlines(),
     scripts=['rst2sile', 'rst2pdf'],
     packages=['sile'],
